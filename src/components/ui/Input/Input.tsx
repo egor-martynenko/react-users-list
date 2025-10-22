cc6a4764f940b970a78fe7d28b2d4c19250a24fb
egormartynenko <egor.martinenko01@gmail.com>
2025-10-22 12:57:56 +0300

    the development of the test task is complete

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -2,7 +2,7 @@ import styles from './Input.module.css'
 import type {ComponentProps, FC} from "react";
 import {SearchIcon} from "../../../assets/svg/SearchIcon.tsx";
 
-export type InputProps = ComponentProps<'input'>
+type InputProps = ComponentProps<'input'>
 
 export const Input:FC<InputProps> = ({placeholder}) => {
   return (
